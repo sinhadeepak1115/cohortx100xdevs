65e6d950243aca4fe3ae0a1a57adb9a8281decb6
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-11 00:56:10 +0530

    adding index.js

--- new-year-app/backend/index.js
+++ new-year-app/backend/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const { createTodo } = require('./types')
-const { updateTodo } = require('./types')
+const { createTodo, updateTodo } = require('./types')
+const { todo } = require('./db')
+
 const port = 3000
 
 app.use(express.json());
 
-app.get('/todo', (req, res) => {
+app.get('/todo', async (req, res) => {
   const createPayLoad = req.body;
   const parsedPayLoad = createTodo.safeParse(createPayLoad);
   if (!parsedPayLoad.success) {
@@ -15,16 +16,42 @@ app.get('/todo', (req, res) => {
     })
     return;
   }
+  await todo.create({
+    title: createPayLoad.title,
+    description: createPayLoad.description,
+    completed: false
+  })
+  res.json({
+    msg: "Todo created"
+  })
 })
 
-app.get("/todos", (req, res) => {
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find();
+  res.json({
+    todos
+  })
 
 })
 
-app.put("/completed", (req, res) => {
-
+app.put("/completed", async (req, res) => {
+  const updatePayLoad = req.body;
+  const parsedPayLoad = updateTodo.safeParse(updatePayLoad);
+  if (!parsedPayLoad.success) {
+    res.status(411).json({
+      msf: "You sent the wrong inputs",
+    })
+    return;
+  }
+  await todo.update({
+    _id: req.body.id
+  }, {
+    completed: true
+  })
+  res.json({
+    msg: "Todo marked as completed."
+  })
 })
-
 
 
 app.listen(port, () => {
