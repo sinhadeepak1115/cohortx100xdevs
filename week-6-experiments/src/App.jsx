88bfc05538dec22e550b6f5ede836a56fcf8bd7f
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-14 02:55:55 +0530

    learned how to use children in CardWrapper.

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,51 +1,15 @@
-import React, { useState, useMemo } from 'react';
-
-let counter = 4;
-
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 3,
-    title: "Deepak",
-    description: "Developer"
-  }, {
-    id: 2,
-    title: "Deep",
-    description: "Developer"
-  }, {
-    id: 1,
-    title: "epak",
-    description: "Developer"
-
-
-  }])
-  function addTodo() {
-    setTodos([...todos, {
-      id: counter++,
-      title: "dskfa",
-      description: "sdjfkjsd"
-    }])
-  }
-
   return (
     <div>
-      <button onClick={addTodo}>Add a todo</button>
-      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+      <CardWrapper>Hi there</CardWrapper>
     </div>
-  );
-}
-
-function Todo({ title, description }) {
-  return (
-    <>
-      <h1>
-        {title}
-      </h1>
-      <h2>
-        {description}
-      </h2>
-    </>
   )
 }
 
-export default App;
+function CardWrapper({ children }) {
+  return <>
+    <div style={{ border: "2px solid black" }}>{children}</div>
+  </>
+}
 
+export default App
