b6dec1c4810125838266a2277a56c73634a4df7c
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-14 06:25:59 +0530

    learnt useEffect

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,15 +1,34 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos")
+        .then(async (res) => {
+          const json = await res.json();
+          setTodos(json.todos);
+        });
+    }, 1000)
+  }, []);
+
   return (
     <div>
-      <CardWrapper>Hi there</CardWrapper>
+      {todos.map((todo) => (
+        <Todo key={todo.key} title={todo.title} description={todo.description} />
+      ))}
     </div>
-  )
+  );
 }
 
-function CardWrapper({ children }) {
-  return <>
-    <div style={{ border: "2px solid black" }}>{children}</div>
-  </>
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h4>{description}</h4>
+    </div>
+  );
 }
 
-export default App
+export default App;
