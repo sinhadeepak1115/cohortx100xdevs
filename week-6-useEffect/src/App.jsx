5033b4c9cf7520c93a39807358e60c64178bb63e
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-14 17:35:12 +0530

    react hooks.

--- week-6-useEffect/src/App.jsx
+++ week-6-useEffect/src/App.jsx
@@ -1,31 +1,38 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+
+
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+
   useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todos")
-      .then(async function(res) {
-        const json = await res.json();
-        setTodos(json.todos);
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(function(response) {
+        setTodos(response.data.todos)
       })
-  }, [todos.id])
+  }, []);
 
-
-  return <div>
-    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
-  </div>
+  return (
+    <>
+      {todos.map(todo => <Todo title={todo.title} description={todo.description} id={todo.id} />)}
+    </>
+  )
 }
 
-function Todo({title, description}) {
-  return <div>
-    <h1>
-      {title}
-    </h1>
-    <h4>
-      {description}
-    </h4>
-  </div>
+function Todo({ id, title, description }) {
+  return (
+    <>
+      <h1>
+        {title}
+      </h1>
+      <h3>{id}</h3>
+      <h4>
+        {description}
+      </h4>
+    </>
+  )
 }
 
 export default App;
