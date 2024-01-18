8e8e7eb4f65622e7edf4e0fe7432d3670e68f3f0
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-18 22:17:30 +0530

    routing.

--- week-7-1/src/App.jsx
+++ week-7-1/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { Dashboard } from "./components/Dashboard";
 import { Landing } from "./components/Landing";
 
 function App() {
   return (
     <>
-      <button onClick={() =>
-        window.location.href = "/dashboard"}>Dashboard</button>
-      <button onClick={() => window.location.href = "/"}>Landing</button>
       <BrowserRouter>
+        <Appbar />
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<Landing />} />
@@ -21,4 +19,22 @@ function App() {
   )
 }
 
+
+function Appbar() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>
+        <button onClick={() => {
+          navigate("/");
+        }}>Landing Page</button>
+
+        <button onClick={() => {
+          navigate("/dashboard");
+        }}>Dashboard Page</button>
+
+      </div>
+    </>
+  )
+}
 export default App;
