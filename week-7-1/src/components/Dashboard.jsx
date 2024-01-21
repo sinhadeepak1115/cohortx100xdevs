e17673ed40775a1f048701e3f1cfe9bacc264d61
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-21 15:51:49 +0530

    latest commit

--- week-7-1/src/components/Dashboard.jsx
+++ week-7-1/src/components/Dashboard.jsx
@@ -2,6 +2,14 @@
 export default function Dashboard() {
   return <div>
     Dashboard Page
+    <Boldify>
+      Deepak
+    </Boldify>
   </div>
 }
 
+function Boldify({ children }) {
+  return <b>
+    {children}
+  </b>
+}
