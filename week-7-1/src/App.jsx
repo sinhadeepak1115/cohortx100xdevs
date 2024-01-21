c3a614b9e98f0edc7b80940b7cde4689c49686a0
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-22 00:06:02 +0530

    learning context api.

--- week-7-1/src/App.jsx
+++ week-7-1/src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
+import { CountContext } from "./context";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <Count count={count} setCount={setCount} />
+      <CountContext.Provider value={count}>
+        <Count count={count} setCount={setCount} />
+      </CountContext.Provider>
     </div>
   )
 }
 function Count({ count, setCount }) {
   return <div>
     {count}
+    <CountRenderer />
     <Button count={count} setCount={setCount} />
   </div>
-
 }
+
+function CountRenderer({ count }) {
+  return <div>
+    {count}
+  </div>
+}
+
 function Button({ count, setCount }) {
   return (
     <div>
