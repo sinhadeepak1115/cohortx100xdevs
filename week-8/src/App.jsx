12c1a7a84f9822582a3668e825897261cbcaa9e1
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-26 21:32:31 +0530

    learning tailwindcss.

---
+++ week-8/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className='flex'>
+        <div style={{ backgroundColor: "red" }}>hi</div>
+        <div style={{ backgroundColor: "tan" }}>di</div>
+        <div style={{ backgroundColor: "aqua" }}>df</div>
+
+      </div >
+    </>
+  )
+} export default App
