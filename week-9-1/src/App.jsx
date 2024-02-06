b3645ad5e21c1a56eeb7de9cc07e296d6f58defa
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-07 02:28:26 +0530

    data hooks for fetching data.

--- week-9-1/src/App.jsx
+++ week-9-1/src/App.jsx
@@ -1,30 +1,52 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import axios from 'axios'
+
+function useTodos(n) {
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState([])
+
+  function getData() {
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      })
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      getData();
+    }, n * 1000)
+    getData();
+  }, [n])
+
+  return {
+    todos: todos,
+    loading: loading
+  };
+}
 
 function App() {
-  const [render, setRender] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setRender(r => !r);
-    }, 10000)
-  }, [])
+  const { todos, loading } = useTodos(5);
+
+  if (loading) {
+    return <div>
+      Loading...
+    </div>
+  }
 
   return (
     <>
-      {render ? <MyComponent /> : <div></div>}
+      {todos.map(todo => <Track todo={todo} />)}
     </>
   )
 }
 
-function MyComponent() {
-  useEffect(() => {
-    console.error("component mounted");
-    return () => {
-      console.log("component unmounted");
-    }
-  }, []);
+function Track({ todo }) {
   return <div>
-    From inside my component
+    {todo.title}
+    <br />
+    {todo.description}
   </div>
 }
 
