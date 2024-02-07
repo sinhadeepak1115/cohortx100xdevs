7290eeab09ec3dc4c72c1084676cce88fafdf7c3
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-08 00:02:56 +0530

    made mouse pointer.

--- week-9-1/src/App.jsx
+++ week-9-1/src/App.jsx
@@ -1,53 +1,30 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function useTodos(n) {
-  const [loading, setLoading] = useState(true);
-  const [todos, setTodos] = useState([])
 
-  function getData() {
-    axios.get("https://sum-server.100xdevs.com/todos")
-      .then(res => {
-        setTodos(res.data.todos);
-        setLoading(false);
-      })
-  }
 
-  useEffect(() => {
-    setInterval(() => {
-      getData();
-    }, n * 1000)
-    getData();
-  }, [n])
-
-  return {
-    todos: todos,
-    loading: loading
+function useMousePointer() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const handelMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
   };
+  useEffect(() => {
+    window.addEventListener('mousemove', handelMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handelMouseMove)
+    }
+
+  }, []);
+  return position;
 }
-
 function App() {
-  const { todos, loading } = useTodos(5);
-
-  if (loading) {
-    return <div>
-      Loading...
-    </div>
-  }
+  const mousePointer = useMousePointer();
 
   return (
     <>
-      {todos.map(todo => <Track todo={todo} />)}
+      Your mouse Positsion is {mousePointer.x} {mousePointer.y}
     </>
   )
-}
-
-function Track({ todo }) {
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
 }
 
 export default App
