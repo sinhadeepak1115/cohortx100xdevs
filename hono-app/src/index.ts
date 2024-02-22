ad87acdcfb15549009cd57ff302bf39ba02a8eda
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-23 02:52:06 +0530

    learned how to use hono.

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -1,14 +1,18 @@
-import { console } from '@cloudflare/workers-types';
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', async (c) => {
-  const body = await c.req.json()
-  console.log(body);
-  console.log(c.req.header("Authoriztion"));
-  console.log(c.req.query("params"));
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono! This is a GET request.');
+});
 
-export default app
+app.post('/', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  return c.text('Hello Hono! This is a POST request.');
+});
+
+export default app;
+
