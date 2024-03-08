bbf8a5e9e1e8b8f710e1db67dbec7628c0ce6d9f
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-03-08 21:38:45 +0530

    learning client side components

--- week-14-1-nextjs/components/Signin.tsx
+++ week-14-1-nextjs/components/Signin.tsx
@@ -1,3 +1,4 @@
+"use client";
 export function SigninComponent() {
   return (
     <div className="h-screen flex justify-center flex-col">
@@ -21,6 +22,9 @@ export function SigninComponent() {
                 placeholder="123456"
               />
               <button
+                onCLick={() => {
+                  console.log("Hello");
+                }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
