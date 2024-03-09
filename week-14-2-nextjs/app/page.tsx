b01cdde06c5807b05f78c906c5605ce6769507d9
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-03-09 20:34:03 +0530

    learning how to add loading.

--- week-14-2-nextjs/app/page.tsx
+++ week-14-2-nextjs/app/page.tsx
@@ -10,9 +10,13 @@ async function getUserDetails() {
 export default async function Home() {
   const userData = await getUserDetails();
   return (
-    <div>
-      {userData.name}
-      {userData.email}
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userData.name}</div>
+          Email: {userData.email}
+        </div>
+      </div>
     </div>
   );
 }
