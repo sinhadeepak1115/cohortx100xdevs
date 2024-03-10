7ea723cd06dec0bc9a06ffca92a715bcde55a9d9
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-03-10 22:17:12 +0530

    making backend for signup

--- week-14-2.1-nextjs/app/api/user/route.ts
+++ week-14-2.1-nextjs/app/api/user/route.ts
@@ -1,7 +1,9 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  console.log(body);
-  return Response.json({ message: "Hello, World!" });
+  return NextResponse.json({
+    username: body.username,
+    password: body.password,
+  });
 }
