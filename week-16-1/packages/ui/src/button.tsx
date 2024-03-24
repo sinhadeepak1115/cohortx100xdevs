dfef8e82dd0d17bbc1e0be3f87d1d8075e8920b6
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-03-24 22:10:58 +0530

    how to use monorepo

--- week-16-1/packages/ui/src/button.tsx
+++ week-16-1/packages/ui/src/button.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = () => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
+    <button onClick={() => alert(`Hello from your app!`)}>Hi there!</button>
   );
 };
