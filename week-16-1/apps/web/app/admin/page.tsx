2d0aaeda3306fae82653fd2bbfa1530923864295
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-03-24 22:23:53 +0530

    learning add a new page

---
+++ week-16-1/apps/web/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import { Admin } from "@repo/ui/admin";
+
+const MyDefaultComponent = () => {
+  return (
+    <div>
+      <h1>Welcome to My Default Component</h1>
+      <Admin />
+    </div>
+  );
+};
+
+// Export the default component
+export default MyDefaultComponent;
---
+++ week-16-1/packages/ui/src/admin.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const Admin = () => {
+  return <h1>hi from admin component</h1>;
+};
