9fb91c85b9d47a4d2002fcb5185b945a03713f4a
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-07 15:00:10 +0530

    checkpoint

--- week-16-nextauth/app/api/auth/[...nextauth]/route.ts
+++ week-16-nextauth/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ export function GET(
   req: NextRequest,
   { params }: { params: { nextauth: string[] } },
 ) {
+  console.log(params.nextauth[0]);
   return NextResponse.json({
     message: "Handler",
   });
