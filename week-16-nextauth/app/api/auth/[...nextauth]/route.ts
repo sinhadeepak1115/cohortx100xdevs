f9d0b957cb4be23c7e33fedcace481c854d5925d
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-07 21:49:27 +0530

    created appbar and nextauth

--- week-16-nextauth/app/api/auth/[...nextauth]/route.ts
+++ week-16-nextauth/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,33 @@
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export function GET(
-  req: NextRequest,
-  { params }: { params: { nextauth: string[] } },
-) {
-  console.log(params.nextauth[0]);
-  return NextResponse.json({
-    message: "Handler",
-  });
-}
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "email",
+          type: "text",
+          placeholder: "email",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "password",
+        },
+        otp: {
+          label: "OTP",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials: any) {
+        return { id: "user1" };
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
---
+++ week-16-nextauth/components/AppBar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export const AppBar = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          router.push("/api/auth/signin");
+        }}
+      >
+        Signin
+      </button>
+    </div>
+  );
+};
